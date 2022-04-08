ce2705e7dc09b0f17f03a551c89d6f49f4692901
JessieCheng <hamy820326@gmail.com>
2022-04-08 13:21:41 +0800

    feat: add user basic, status table and seeds

---
+++ app/Database/Migrations/2022-04-08-022404_AddStatusTable.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class AddStatusTable extends Migration
+{
+	protected $DBGroup = 'member';
+
+	public function up()
+	{
+		$this->forge->addField([
+			'id',
+			'name' => [
+				'type' => 'VARCHAR',
+				'constraint' => 10,
+				'comment' => '名稱'
+			],
+			'created_at datetime DEFAULT current_timestamp NOT NULL COMMENT \'建立時間\'',
+			'updated_at datetime DEFAULT current_timestamp NOT NULL ON update current_timestamp COMMENT \'更新時間\'',
+		]);
+
+		$this->forge->createTable('status', true, [
+			'ENGINE' => 'InnoDB',
+			'CHARACTER SET' => 'utf8mb4',
+			'COLLATE' => 'utf8mb4_unicode_ci',
+			'comment' => '使用者狀態一覽'
+		]);
+	}
+
+	public function down()
+	{
+		$this->forge->dropTable('status', true);
+	}
+}
