2aa5311b1ec0092e09bf10942687a36dd955509b
JessieCheng <hamy820326@gmail.com>
2022-04-08 17:15:47 +0800

    feat: add data rule validation in Api controller
    rather than validate in each controller

--- app/Controllers/ApiController.php
+++ app/Controllers/ApiController.php
@@ -4,6 +4,8 @@ namespace App\Controllers;
 
 use CodeIgniter\HTTP\IncomingRequest;
 use CodeIgniter\HTTP\ResponseInterface;
+use Config\Services; // All of the core classes within CodeIgniter are provided as "services"
+use CodeIgniter\Validation\Exceptions\ValidationException;
 
 class ApiController extends BaseController
 {
@@ -23,5 +25,33 @@ class ApiController extends BaseController
       ->response
       ->setStatusCode($code)
       ->setJSON($responseBody);
+  }
+
+  // Validate the input parse by getRequestInput by rules that set in Controllers/.
+  public function validateRequest(array $input, array $rules, array $messages = []): bool
+  {
+    $this->validator = Services::Validation()->setRules($rules);
+
+    // TODO: Don't understand this meaning in if block.
+    // If you replace the $rules array with the name of the group
+
+    // if (is_string($rules)) {
+    //   $validation = config('Validation');
+
+    //   // If the rule wasn't found in the \Config\Validation, we should throw an exception so the developer can find it.
+    //   if (!isset($validation->$rules)) {
+    //     throw ValidationException::forRuleNotFound($rules);
+    //   }
+
+    //   // If no error message is defined, use the error message in the Config\Validation file
+    //   if (!$messages) {
+    //     $errorName = $rules . '_errors';
+    //     $messages = $validation->$errorName ?? [];
+    //   }
+
+    //   $rules = $validation->$rules;
+    // }
+
+    return $this->validator->setRules($rules, $messages)->run($input);
   }
 }
